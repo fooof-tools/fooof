3dbe5957ef918f0d0c4c20dacb7644122c65a70f
TomDonoghue <tdonoghue@ucsd.edu>
2017-08-01 12:59:34 -0700

    Add sketch of rest of test files

---
+++ fooof/tests/test_fooof.py
@@ -0,0 +1,40 @@
+"""   """
+
+from fooof import FOOOF
+
+##
+##
+
+def test_FOOOF():
+
+	assert FOOOF(2, 2, [2, 2])
+
+def test_drop_osc_overlap():
+	pass
+
+def test_drop_osc_bw():
+	pass
+
+def test_drop_osc_cf():
+	pass
+
+def test_drop_osc_amp():
+	pass
+
+def test_fit_osc_guess():
+	pass
+
+def test_check_oscs():
+	pass
+
+def test_fit_oscs():
+	pass
+
+def test_clean_backgroud_fit():
+	pass
+
+def test_quick_background_fit():
+	pass
+
+def test_fit():
+	pass
---
+++ fooof/tests/test_funcs.py
@@ -0,0 +1,15 @@
+"""   """
+
+from fooof.funcs import gaussian_function, linear_function, quadratic_function
+
+##
+##
+
+def test_gaussian_function():
+	pass
+
+def test_linear_function():
+	pass
+
+def test_quadratic_function():
+	pass
