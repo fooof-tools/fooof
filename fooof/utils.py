bd89b3949779deecdc2ab2486cc124f1ca6e5beb
TomDonoghue <tdonoghue@ucsd.edu>
2017-08-16 21:22:48 -0700

    Clean up, remove old code

--- fooof/utils.py
+++ fooof/utils.py
@@ -10,7 +10,7 @@ def overlap(lst_1, lst_2):
 
     Parameters
     ----------
-    a, b : list of [float, float]
+    lst_1, lst_2 : list of [float, float]
         Boundary definitions.
 
     Returns
@@ -88,62 +88,3 @@ def trim_psd(freqs, psd, f_range):
     psd_ext = psd[f_mask]
 
     return freqs_ext, psd_ext
-
-
-# def get_index_from_vector(input_vector, element_value):
-#     """Returns index for input closest to desired element value.
-
-#     Parameters
-#     ----------
-#     input_vector : 1d array
-#         Vector of values to search through.
-#     element_value : float
-#         Value to search for in input vector.
-
-#     Returns
-#     -------
-#     idx : int
-#         Index closest to element value.
-#     """
-
-#     loc = input_vector - element_value
-#     idx = np.where(np.abs(loc) == np.min(np.abs(loc)))
-
-#     return idx[0][0]
-
-
-# def trim_psd(input_frequency_vector, input_psd, frequency_range):
-#     """Extract PSD, and frequency vector, to desired frequency range.
-
-#     Parameters
-#     ----------
-#     input_frequency_vector :
-#         Frequency values for the PSD.
-#     input_psd : 2d array
-#         Power spectral density values.
-#     frequency_range : list of [float, float]
-#         Desired frequency range of PSD.
-
-#     Returns
-#     -------
-#     output_frequency_vector : 1d array
-#         Extracted frequency values for the PSD.
-#     trimmed_psd :
-#         Extracted power spectral density values.
-
-#     NOTEs (TODO):
-#     This function currently assumes 2d input. Should it?
-#         In particular, it assumes column vectors of PSDs.
-#     It also (using get_index_from_vector) round bi-directionally.
-#         Not obvious, and maybe not desirable.
-#     It also, also: doesn't include last requested frequency.
-#         So f_range [2, 30] would include f_vals 2 to one f_val prior to 30.
-#         With the rounding: this happens
-#     """
-
-#     idx = [get_index_from_vector(input_frequency_vector, freq) for freq in frequency_range]
-
-#     output_frequency_vector = input_frequency_vector[idx[0]:idx[1]]
-#     trimmed_psd = input_psd[idx[0]:idx[1]]#, :]
-
-#     return output_frequency_vector, trimmed_psd
