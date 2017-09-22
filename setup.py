faa1deb17a5da13ace71e00deaee85e73f47a64c
TomDonoghue <tdonoghue@ucsd.edu>
2017-09-21 22:10:00 -0700

    add files to be PYPI ready

---
+++ setup.py
@@ -0,0 +1,17 @@
+"""FOOOF setup script."""
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'fooof',
+    version = '0.1',
+    description = 'Fitting oscillations & one-over f',
+    author = 'The Voytek Lab',
+    author_email = 'voyteklab@gmail.com',
+    url = 'https://github.com/voytekresearch/fooof',
+    packages=find_packages(),
+    license='MIT',
+    download_url = '',
+    keywords = ['neuroscience', 'neural oscillations', 'power spectra'],
+    classifiers = []
+)
