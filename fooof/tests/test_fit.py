14e6c7a054cdd663b62da3cf915bd6b17ae7ca6d
TomDonoghue <tdonoghue@ucsd.edu>
2017-11-23 22:36:21 -0800

    Add test suite for FOOOF, and fit

--- fooof/tests/test_fit.py
+++ fooof/tests/test_fit.py
@@ -1,108 +1,129 @@
-"""Tests for the FOOOF module."""
+"""Tests for the FOOOF fit objects and modules.
+
+NOTES
+-----
+The tests here are not strong tests for accuracy.
+	They serve rather as 'smoke tests', for if anything fails completely.
+"""
+
+from py.test import raises
 
 import numpy as np
+import pkg_resources as pkg
 
-from fooof import FOOOF
-from fooof.funcs import gaussian_function, quadratic_function
+from fooof import FOOOF, FOOOFGroup
+from fooof.tests.utils import mk_fake_data, mk_fake_group_data
 
 ##########################################################################################
 ##########################################################################################
 
-def mk_fake_data(xs, bgp, oscs):
-	"""Create fake PSD for testing.
-
-	Parameters
-	----------
-	xs : 1d array
-		Frequency vector to create fake PSD with.
-	bgp : list of [float, float, float]
-		Parameters to create the background of PSD.
-	oscs : list of [float, float, float]
-		Parameters to create oscillations. Length of n_oscs * 3.
-
-	Returns
-	-------
-	xs : 1d array
-		Frequency values (linear).
-	ys : 1d array
-		Power values (linear).
-	"""
-
-	bg = quadratic_function(np.log10(xs), *bgp)
-	oscs = gaussian_function(xs, *oscs)
-	noise = np.random.normal(0, 0.005, len(xs))
-
-	ys = np.power(10, bg + oscs + noise)
-
-	return xs, ys
-
-def test_FOOOF():
+def test_fooof():
 	"""Check FOOOF object initializes properly."""
 
 	assert FOOOF()
 
-def test_model():
-	"""Test the model test.
-	Note: this is (sort of) an 'integration' test - model runs almost every other method.
-	"""
+def test_fooof_fit_nk():
+	"""Test FOOOF fit, no knee."""
 
-	xs = np.arange(3, 40, 0.5)
-	bgp = [-20, -0.8, 3e-15]
+	xs = np.arange(3, 50, 0.5)
+	bgp = [50, 2]
 	oscs = [[10, 0.5, 2],
 			[20, 0.3, 4]]
 
 	xs, ys = mk_fake_data(xs, bgp, [it for osc in oscs for it in osc])
 
-	ff = FOOOF()
-
-	ff.model(xs, ys, [3, 40])
+	fm = FOOOF()
+	fm.fit(xs, ys)
 
 	# Check model results - background parameters
-	assert np.all(np.isclose(bgp, ff.background_params, [0.5, 0.1, 1e-16]))
+	assert np.all(np.isclose(bgp, fm.background_params_, [0.5, 0.1]))
 
 	# Check model results - gaussian parameters
 	for i, osc in enumerate(oscs):
-		assert np.all(np.isclose(osc, ff._gaussian_params[i], [1, 0.2, 0.5]))
+		assert np.all(np.isclose(osc, fm._gaussian_params[i], [1, 0.2, 0.5]))
 
-def test_reset_dat():
+def test_fooof_checks():
+	"""Test various checks, errors and edge cases in FOOOF."""
 
-	ff = FOOOF()
+	xs, ys = mk_fake_data(np.arange(3, 50, 0.5), [50, 2], [10, 0.5, 2])
 
-	ff.model(*mk_fake_data(np.arange(3, 40, 0.5), [-20, -0.8, 3e-15], [10, 0.5, 2, 20, 0.3, 4]), [3, 40])
-	ff._reset_dat()
+	fm = FOOOF()
 
-	assert ff.freqs is None and ff.psd is None and ff.freq_range is None \
-		and ff.freq_res is None and ff.psd_fit is None and ff.background_params is None \
-		and ff.oscillation_params is None and ff.error is None \
-		and ff._psd_flat is None and ff._psd_osc_rm is None and ff._gaussian_params is None \
-		and ff._background_fit is None and ff._oscillation_fit is None
+	# Check dimension error
+	with raises(ValueError):
+		fm.fit(xs[:-1], ys)
 
-# def test_fit():
-# 	pass
+	# Check shape mismatch error
+	with raises(ValueError):
+		fm.fit(xs, np.reshape(ys, [1, len(ys)]))
 
-# def test_plot():
-# 	pass
+	# Check trim_psd range
+	fm.fit(xs, ys, [3, 40])
 
-# def test_print_params():
-# 	pass
+	# Check freq of 0 issue
+	xs, ys = mk_fake_data(np.arange(0, 50, 0.5), [50, 2], [10, 0.5, 2])
+	fm.fit(xs, ys)
 
-# def test_drop_osc_overlap():
-# 	pass
+	# Check plot and string report model error
+	fm = FOOOF()
+	with raises(ValueError):
+		fm.print_results()
+	with raises(ValueError):
+		fm.plot()
 
-# def test_drop_osc_bw():
-# 	pass
+def test_fooof_prints_plot_get_report():
+	"""Test methods that print, plot, return run through.
 
-# def test_drop_osc_cf():
-# 	pass
+	Checks: print_settings, print_results, plot, get_results, create_report
 
-# def test_r_squared():
-# 	pass
+	Note: minimal test - that methods run. No accuracy checking.
+	"""
 
-# def test_fit_oscs():
-# 	pass
+	xs, ys = mk_fake_data(np.arange(3, 50, 0.5), [50, 2], [10, 0.5, 2, 20, 0.3, 4])
 
-# def test_clean_backgroud_fit():
-# 	pass
+	fm = FOOOF()
 
-# def test_quick_background_fit():
-# 	pass
+	fm.print_settings()
+
+	fm.fit(xs, ys)
+
+	fm.print_results()
+
+	fm.plot()
+
+	out = fm.get_results()
+	assert out
+
+	fm.create_report(save_path=pkg.resource_filename(__name__, 'reports'))
+
+def test_fooof_reset_dat():
+	"""Check that all relevant data is cleared in the resest method."""
+
+	fm = FOOOF()
+
+	fm.model(*mk_fake_data(np.arange(3, 50, 0.5), [50, 2], [10, 0.5, 2, 20, 0.3, 4]))
+	fm._reset_dat()
+
+	assert fm.freqs is None and fm.psd is None and fm.freq_range is None \
+		and fm.freq_res is None and fm.psd_fit_ is None and fm.background_params_ is None \
+		and fm.oscillation_params_ is None and fm.r2_ is None and fm.error_ is None \
+		and fm._psd_flat is None and fm._psd_osc_rm is None and fm._gaussian_params is None \
+		and fm._background_fit is None and fm._oscillation_fit is None
+
+def test_fooof_group():
+	"""Check FOOOFGroup object initializes properly."""
+
+	assert FOOOFGroup()
+
+def test_fooof_group_fit():
+	"""Test FOOOFGroup fit, no knee."""
+
+	xs, ys = mk_fake_group_data(np.arange(3, 50, 0.5))
+
+	fg = FOOOFGroup()
+
+	fg.fit_group(xs, ys)
+
+	out = fg.get_group_results()
+
+	assert out
