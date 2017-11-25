8828a9d9d3b3adf780ee40951c016acf2f4a5283
TomDonoghue <tdonoghue@ucsd.edu>
2017-11-25 12:39:20 -0800

    Add dict utils for file IO

--- fooof/utils.py
+++ fooof/utils.py
@@ -1,4 +1,4 @@
-"""Util functions for FOOOF."""
+"""Basic utility functions for FOOOF."""
 
 import numpy as np
 
@@ -23,6 +23,61 @@ def group_three(vec):
         raise ValueError('Wrong size array to group by three.')
 
     return [list(vec[i:i+3]) for i in range(0, len(vec), 3)]
+
+
+def dict_array_to_lst(in_dict):
+    """Convert any numpy arrays present in a dictionary to be lists.
+
+    Parameters
+    ----------
+    in_dict : dict
+        Input dictionary.
+
+    Returns
+    -------
+    dict
+        Output dictionary with all arrays converted to lists.
+    """
+
+    return {ke: va.tolist() if isinstance(va, np.ndarray) else va for ke, va in in_dict.items()}
+
+
+def dict_lst_to_array(in_dict, mk_array):
+    """Convert specified lists in a dictionary to be arrays.
+
+    Parameters
+    ----------
+    in_dict : dict
+        Input dictionary.
+    mk_array : list of str
+        Keys to convert to arrays in the dictionary.
+
+    Returns
+    -------
+    dict
+        Output dictionary with specified lists converted to arrays.
+    """
+
+    return {ke: np.array(va) if ke in mk_array else va for ke, va in in_dict.items()}
+
+
+def dict_select_keys(in_dict, keep):
+    """Restrict a dictionary to only keep specified keys.
+
+    Parameters
+    ----------
+    in_dict : dict
+        Input dictionary.
+    keep : list or set
+        Keys to retain in the dictionary.
+
+    Returns
+    -------
+    dict
+        Output dictionary containing only keys specified in keep.
+    """
+
+    return {ke:va for ke, va in in_dict.items() if ke in keep}
 
 
 def trim_psd(freqs, psd, f_range):
