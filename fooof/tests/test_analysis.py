d82867f1a04debcda182debb33110467c8daad9b
Luyanda Mdanda <lmdanda@ucsd.edu>
2017-12-06 18:36:12 -0800

    improved tests

--- fooof/tests/test_analysis.py
+++ fooof/tests/test_analysis.py
@@ -8,15 +8,15 @@ from fooof.analysis import *
 ###################################################################################################
 ###################################################################################################
 
-def test_get_highest_power_osc():
-
-	dat = np.array([[10, 1, 1.8],[14, 2, 4]])
-	assert get_band_osc(dat, [10, 12]) == [10, 1, 1.8]
-	assert get_band_osc(dat, [4, 8]) == np.array([np.nan, np.nan, np.nan])
-	assert get_band_osc(dat, [10, 14], ret_one=False) == [[10, 1, 1.8],[14, 2, 4]]
-
-
 def test_get_band_osc():
 
-	dat = np.array([[10, 1, 1.8],[14, 2, 4],[12, 3, 2]])
-	assert np.array_equal(p_out, np.array([12, 3, 2]))
+    dat = np.array([[10, 1, 1.8],[14, 2, 4]])
+    assert np.array_equal(get_band_osc(dat,[10, 12]),[10, 1, 1.8])
+    assert np.all(np.isnan(get_band_osc(dat, [4, 8]))) 
+    assert np.array_equal(get_band_osc(dat, [10, 14], ret_one=False),[[10, 1, 1.8],[14, 2, 4]])
+
+
+def test_get_highest_power_osc():
+
+    dat = np.array([[10, 1, 1.8],[14, 2, 4],[12, 3, 2]])
+    assert np.array_equal(get_highest_power_osc(dat),[12, 3, 2])
