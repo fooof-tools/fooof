085cbbdf51bd2262e676853e8a5aa50e6d163fb7
Luyanda Mdanda <lmdanda@ucsd.edu>
2017-12-06 19:24:22 -0800

    patches

--- fooof/analysis.py
+++ fooof/analysis.py
@@ -6,37 +6,37 @@ import numpy as np
 ###################################################################################################
 
 def get_band_osc(osc_params, band_def, ret_one=True):
-    """Searches for a given band of interest within a list of oscillation
-    
+    """Searches for oscillations within a given band of interest.
+
     Parameters
     ----------
     osc_params : 2d array
-        Oscillations parameters, from FOOOF. [n_oscs, 3] 
+        Oscillations parameters, from FOOOF. [n_oscs, 3]
     band_def : [float, float]
         Defines the band of interest
     ret_one : bool
         Whether to return single oscillation (or all found)
-        
+
     Return
     ---------
-    osc_out : array
-        Osc data, form - (centers, powers, bws, # oscillations).  
+    band_oscs : array
+        Osc data, form - (centers, powers, bws.
     """
-    
+
     # Catch & return if empty
     if not np.all(osc_params):
         return [np.nan, np.nan, np.nan]
-    
+
     # Find indices of oscillations in the specified range
     osc_inds = (osc_params[:, 0] >= band_def[0]) & (osc_params[:, 0] <= band_def[1])
-    
+
     # Gets the number of oscillations within the specified range
     n_oscs = sum(osc_inds)
-    
-    # If there are no oscillation within the specified range 
+
+    # If there are no oscillation within the specified range
     if n_oscs == 0:
         return np.array([np.nan, np.nan, np.nan])
-    
+
     band_oscs = osc_params[osc_inds, :]
 
     # If results > 1 and ret_one, then we return the highest power oscillation
@@ -44,28 +44,28 @@ def get_band_osc(osc_params, band_def, ret_one=True):
     if n_oscs > 1 and ret_one:
         # Get highest power oscillation in band
         band_oscs = get_highest_power_osc(band_oscs)
-    
+
     # If results == 1, return osc - [cen, power, bw]
     return np.squeeze(band_oscs)
 
 
 def get_highest_power_osc(band_oscs):
-    """Searches for the highest power oscillation within a band of interest
-    
+    """Searches for the highest power oscillation.
+
     Parameters
     ----------
     osc_params : 2d array
-        Oscillations parameters, from FOOOF. [n_oscs, 3] 
-        
+        Oscillations parameters, from FOOOF. [n_oscs, 3]
+
     Return
     ---------
     band_oscs : array
-        Osc data, form - (centers, powers, bws, # oscillations).  
+        Osc data, form - (centers, powers, bws).
     """
-    
+
     # Catch & return if empty
     if not np.all(band_oscs):
         return [np.nan, np.nan, np.nan]
-    
+
     high_ind = np.argmax(band_oscs[:, 1])
     return band_oscs[high_ind, :]
--- fooof/tests/test_analysis.py
+++ fooof/tests/test_analysis.py
@@ -1,6 +1,5 @@
 """Test functions for FOOOF analysis."""
 
-
 import numpy as np
 
 from fooof.analysis import *
@@ -12,7 +11,7 @@ def test_get_band_osc():
 
     dat = np.array([[10, 1, 1.8],[14, 2, 4]])
     assert np.array_equal(get_band_osc(dat,[10, 12]),[10, 1, 1.8])
-    assert np.all(np.isnan(get_band_osc(dat, [4, 8]))) 
+    assert np.all(np.isnan(get_band_osc(dat, [4, 8])))
     assert np.array_equal(get_band_osc(dat, [10, 14], ret_one=False),[[10, 1, 1.8],[14, 2, 4]])
 
 
