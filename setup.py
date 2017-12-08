6b9a5b93f7cfdff4d7a3d5fd4d4d55112d2cea57
TomDonoghue <tdonoghue@ucsd.edu>
2017-12-07 23:48:11 -0800

    Fix version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'fooof',
-    version = '0.1',
+    version = '0.1.0',
     description = 'Fitting oscillations & one-over f',
     author = 'The Voytek Lab',
     author_email = 'voyteklab@gmail.com',
@@ -12,6 +12,6 @@ setup(
     packages=find_packages(),
     license='MIT',
     download_url = '',
-    keywords = ['neuroscience', 'neural oscillations', 'power spectra'],
+    keywords = ['neuroscience', 'neural oscillations', 'power spectra', '1/f', 'electrophysiology'],
     classifiers = []
 )
