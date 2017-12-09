05cc769cebb38ff81d3427807c3b7690a444f464
TomDonoghue <tdonoghue@ucsd.edu>
2017-12-09 14:17:23 -0800

    Add version number inside module - single place

--- setup.py
+++ setup.py
@@ -2,16 +2,32 @@
 
 from setuptools import setup, find_packages
 
+# Get the current version number from inside the module
+with open(os.path.join('fooof', 'version.py')) as vf:
+    exec(vf.read())
+
 setup(
     name = 'fooof',
-    version = '0.1.0',
+    version = __version__,
     description = 'Fitting oscillations & one-over f',
     author = 'The Voytek Lab',
     author_email = 'voyteklab@gmail.com',
     url = 'https://github.com/voytekresearch/fooof',
     packages=find_packages(),
     license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ]
     download_url = '',
-    keywords = ['neuroscience', 'neural oscillations', 'power spectra', '1/f', 'electrophysiology'],
-    classifiers = []
+    keywords = ['neuroscience', 'neural oscillations', 'power spectra', '1/f', 'electrophysiology']
 )
