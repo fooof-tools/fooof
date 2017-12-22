e0fbb1876e76ea12204466dd203526b748a87324
TomDonoghue <tdonoghue@ucsd.edu>
2017-12-21 23:45:20 -0500

    Fix up to use new synth approach

--- fooof/tests/utils.py
+++ fooof/tests/utils.py
@@ -1,9 +1,7 @@
 """Utilities for testing fooof."""
 
 from fooof import FOOOF, FOOOFGroup
-from fooof.synth import mk_fake_data, mk_fake_group_data
-from fooof.utils import mk_freq_vector
-from fooof.core.modutils import safe_import
+from fooof.synth import gen_power_spectrum, gen_group_power_spectra
 
 ###################################################################################################
 ###################################################################################################
@@ -11,7 +9,11 @@ from fooof.core.modutils import safe_import
 def get_tfm():
     """Get a FOOOF object, with a fit PSD, for testing."""
 
-    xs, ys = mk_fake_data(mk_freq_vector([3, 50], 0.5), [50, 2], [10, 0.5, 2, 20, 0.3, 4])
+    freq_range = [3, 50]
+    bg_params = [50, 2]
+    gauss_params = [10, 0.5, 2, 20, 0.3, 4]
+
+    xs, ys = gen_power_spectrum(freq_range, bg_params, gauss_params)
 
     tfm = FOOOF()
     tfm.fit(xs, ys)
@@ -21,17 +23,19 @@ def get_tfm():
 def get_tfg():
     """Get a FOOOFGroup object, with some fit PSDs, for testing."""
 
-    xs, ys = mk_fake_group_data(mk_freq_vector([3, 50], 0.5), 2)
+    n_psds = 2
+    xs, ys = gen_group_power_spectra(n_psds, *default_group_params())
 
     tfg = FOOOFGroup()
     tfg.fit(xs, ys)
 
     return tfg
 
-# def check_mpl():
-#     """   """
+def default_group_params():
+    """Create default parameters for generating a test group of power spectra."""
 
-#     if safe_import('matplotlib'):
-#         return True
-#     else:
-#         return False
+    freq_range = [3, 50]
+    bgp_opts = [[20, 2], [50, 2.5], [35, 1.5]]
+    gauss_opts = [[], [10, 0.5, 2], [10, 0.5, 2, 20, 0.3, 4]]
+
+    return freq_range, bgp_opts, gauss_opts
