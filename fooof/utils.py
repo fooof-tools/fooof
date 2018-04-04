b47db2eb29b54442715f575cefda4fede48eae15
TomDonoghue <tdonoghue@ucsd.edu>
2018-04-04 14:33:28 -0700

    Add new functions to extract, compare settings, and to combine FOOOF and FOOOFGroup objects

--- fooof/utils.py
+++ fooof/utils.py
@@ -2,6 +2,9 @@
 
 import numpy as np
 
+from fooof import FOOOFGroup
+from fooof.core.modutils import get_obj_desc
+
 ###################################################################################################
 ###################################################################################################
 
@@ -35,7 +38,111 @@ def trim_spectrum(freqs, power_spectra, f_range):
 
     # Restrict freqs & psd to requested range
     freqs_ext = freqs[f_mask]
+    # The if/else is to cover both 1d or 2d arrays
     power_spectra_ext = power_spectra[f_mask] if power_spectra.ndim == 1 \
         else power_spectra[:, f_mask]
 
     return freqs_ext, power_spectra_ext
+
+
+def get_settings(f_obj):
+    """Get a dictionary of current settings from a FOOOF or FOOOFGroup object.
+
+    Parameters
+    ----------
+    f_obj : FOOOF or FOOOFGroup
+        FOOOF derived object to get settings from.
+
+    Returns
+    -------
+    settings : dictionary
+        Settings for the input FOOOF derived object.
+    """
+
+    settings = {}
+    for setting in get_obj_desc()['settings']:
+        settings[setting] = getattr(f_obj, setting)
+
+    return settings
+
+
+def compare_settings(lst):
+    """Compare the settings between FOOOF and/or FOOOFGroup objects.
+
+    Parameters
+    ----------
+    lst : list of FOOOF or FOOOFGroup objects
+        FOOOF related objects whose settings are to be compared.
+
+    Returns
+    -------
+    bool
+        Whether the settings are consistent across the input list of objects.
+    """
+
+    # Runs through all settings, and
+    for setting in get_obj_desc()['settings']:
+        for ind, f_obj in enumerate(lst[:-1]):
+
+            # If any setting fails comparison, return that objects are incompatible
+            if getattr(f_obj, setting) != getattr(lst[ind+1], setting):
+                return False
+
+    # If no settings fail comparison, return that objects have consistent settings
+    return True
+
+
+def combine_fooofs(fooofs):
+    """Combine a group of FOOOF objects into a single FOOOFGroup object.
+
+    Parameters
+    ----------
+    fooofs : list of FOOOF objects
+        FOOOF objects to be concatenated into a FOOOFGroup.
+
+    Returns
+    -------
+    fg : FOOOFGroup object
+        Resultant FOOOFGroup object created from input FOOOFs.
+    """
+
+    # Compare settings
+    if not compare_settings(fooofs):
+        raise ValueError('These objects have incompatible settings, and so cannot be combined.')
+
+    # Initialize FOOOFGroup object
+    fg = FOOOFGroup(**get_settings(fooofs[0]), verbose=fooofs[0].verbose)
+
+    # Add FOOOF results from each FOOOF object to group
+    for fm in fooofs:
+        fg.group_results.append(fm.get_results())
+
+    return fg
+
+
+def combine_fooof_groups(fooof_groups):
+    """Combine a group of FOOOFGroup objects into a new combined FOOOFGroup object.
+
+    Parameters
+    ----------
+    fooof_groups : list of FOOOFGroup objects
+        FOOOFGroup objectst to be concatenated into a single new FOOOFGroup object.
+
+    Returns
+    -------
+    fg : FOOOFGroup object
+        New FOOOFGroup object built from input FOOOFGroups.
+    """
+
+    # Compare settings
+    if not compare_settings(fooof_groups):
+        raise ValueError('These objects have incompatible settings, and so cannot be combined.')
+
+    # Initialize FOOOFGroup object
+    fg = FOOOFGroup(**get_settings(fooof_groups[0]), verbose=fooof_groups[0].verbose)
+
+    # Add FOOOF results from each FOOOF object to group
+    for tfg in fooof_groups:
+        fg.group_results.extend(tfg.group_results)
+
+    return fg
