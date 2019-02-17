722862e1de92ca4138c42b5b20c4ff077556a653
TomDonoghue <tdonoghue@ucsd.edu>
2019-02-17 12:00:39 -0800

    Add & refactor transform psd funcs, including with updating of Synparams

--- fooof/synth/transform.py
+++ fooof/synth/transform.py
@@ -2,6 +2,8 @@
 
 import numpy as np
 
+from fooof.synth.params import update_syn_ap_params
+
 ###################################################################################################
 ###################################################################################################
 
@@ -15,11 +17,11 @@ def rotate_spectrum(freqs, power_spectrum, delta, f_rotation):
     power_spectrum : 1d array
         Power values of the spectrum that is to be rotated.
     delta : float
-        Change in power law exponent to be applied.
+        Change in aperiodic exponent to be applied.
         Positive is counterclockwise rotation (flatten).
         Negative is clockwise rotation (steepen).
     f_rotation : float
-        Frequency (Hz) at which to do the rotation, such that power at that frequency is unchanged.
+        Frequency value, in Hz, about which rotation is applied, at which power is unchanged.
 
     Returns
     -------
@@ -56,7 +58,82 @@ def translate_spectrum(power_spectrum, delta):
     return translated_spectrum
 
 
+def rotate_syn_spectrum(freqs, power_spectrum, delta, f_rotation, syn_params):
+    """Rotate a power spectrum about a frequency point, changing the power law exponent.
+
+    Parameters
+    ----------
+    freqs : 1d array
+        Frequency axis of input power spectrum, in Hz.
+    power_spectrum : 1d array
+        Power values of the spectrum that is to be rotated.
+    delta : float
+        Change in aperiodic exponent to be applied.
+        Positive is counterclockwise rotation (flatten).
+        Negative is clockwise rotation (steepen).
+    f_rotation : float
+        Frequency value, in Hz, about which rotation is applied, at which power is unchanged.
+    syn_params : SynParams object
+        Object storing the current parameter definitions.
+
+    Returns
+    -------
+    rotated_spectrum : 1d array
+        Rotated power spectrum.
+    new_syn_params : SynParams object
+        Updated object storing the new parameter definitions.
+    """
+
+    rotated_spectrum = rotate_spectrum(freqs, power_spectrum, delta, f_rotation)
+    delta_offset = calc_rot_offset(delta, f_rotation)
+
+    new_syn_params = update_syn_ap_params(syn_params, [delta_offset, delta])
+
+    return rotated_spectrum, new_syn_params
+
+
+def translate_syn_spectrum(power_spectrum, delta, syn_params):
+    """Translate a spectrum, changing the offset value.
+
+    Parameters
+    ----------
+    power_spectrum : 1d array
+        Power values of the spectrum that is to be translated.
+    delta : float
+        Amount to change the offset by.
+        Positive is an upwards translation.
+        Negative is a downwards translation.
+    syn_params : SynParams object
+        Object storing the current parameter definitions.
+
+    Returns
+    -------
+    translated_spectrum : 1d array
+        Translated power spectrum.
+    new_syn_params : SynParams object
+        Updated object storing the new parameter definitions.
+    """
+
+    translated_spectrum = translate_spectrum(power_spectrum, delta)
+    new_syn_params = update_syn_ap_params(syn_params, delta, 'intercept')
+
+    return translated_spectrum, new_syn_params
+
+
 def calc_rot_offset(delta, f_rotation):
-    """Calculate the change in offset from a given rotation"""
+    """Calculate the change in offset from a given rotation.
+
+    Parameters
+    ----------
+    delta : float
+        Change in aperiodic exponent to be applied.
+    f_rotation : float
+        Frequency value, in Hz, about which rotation is applied, at which power is unchanged.
+
+    Returns
+    -------
+    float
+        The amount the offset will change for the specified exponent change.
+    """
 
     return -np.log10(f_rotation) * delta
--- fooof/tests/test_synth_transform.py
+++ fooof/tests/test_synth_transform.py
@@ -3,6 +3,7 @@
 import numpy as np
 
 from fooof.synth.gen import gen_power_spectrum
+from fooof.synth.params import SynParams
 from fooof.synth.transform import *
 
 ###################################################################################################
@@ -33,3 +34,26 @@ def test_translate_spectrum():
     # Check that 0 translation returns the same spectrum
     translated_spectrum = translate_spectrum(spectrum, delta=0.)
     assert np.all(translated_spectrum == spectrum)
+
+def test_rotate_syn_spectrum():
+
+    syn_params = SynParams([1, 1], [10, 0.5, 1], 0)
+    freqs, spectrum = gen_power_spectrum([3, 40], *syn_params)
+
+    rotated_spectrum, new_syn_params = rotate_syn_spectrum(freqs, spectrum, 0.5, 20, syn_params)
+
+    assert not np.all(rotated_spectrum == spectrum)
+    assert new_syn_params.aperiodic_params[1] == 1.5
+
+def test_translate_syn_spectrum():
+
+    syn_params = SynParams([1, 1], [10, 0.5, 1], 0)
+    freqs, spectrum = gen_power_spectrum([3, 40], *syn_params)
+
+    translated_spectrum, new_syn_params = translate_syn_spectrum(spectrum, 0.5, syn_params)
+    assert not np.all(translated_spectrum == spectrum)
+    assert new_syn_params.aperiodic_params[0] == 1.5
+
+def cal_rot_offset():
+
+    assert calc_rot_offset(20, 0.5)
