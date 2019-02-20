f6107e4bae2c2927e4f2a22e613ecf648b54240e
TomDonoghue <tdonoghue@ucsd.edu>
2019-02-20 13:52:16 -0800

    Update names of delta vars

--- fooof/synth/transform.py
+++ fooof/synth/transform.py
@@ -7,7 +7,7 @@ from fooof.synth.params import update_syn_ap_params
 ###################################################################################################
 ###################################################################################################
 
-def rotate_spectrum(freqs, power_spectrum, delta, f_rotation):
+def rotate_spectrum(freqs, power_spectrum, delta_exponent, f_rotation):
     """Rotate a power spectrum about a frequency point, changing the power law exponent.
 
     Parameters
@@ -16,7 +16,7 @@ def rotate_spectrum(freqs, power_spectrum, delta, f_rotation):
         Frequency axis of input power spectrum, in Hz.
     power_spectrum : 1d array
         Power values of the spectrum that is to be rotated.
-    delta : float
+    delta_exponent : float
         Change in aperiodic exponent to be applied.
         Positive is clockwise rotation (steepen).
         Negative is counterclockwise rotation (flatten).
@@ -29,20 +29,20 @@ def rotate_spectrum(freqs, power_spectrum, delta, f_rotation):
         Rotated power spectrum.
     """
 
-    mask = (np.abs(freqs) / f_rotation)**-delta
+    mask = (np.abs(freqs) / f_rotation)**-delta_exponent
     rotated_spectrum = mask * power_spectrum
 
     return rotated_spectrum
 
 
-def translate_spectrum(power_spectrum, delta):
+def translate_spectrum(power_spectrum, delta_offset):
     """Translate a spectrum, changing the offset value.
 
     Parameters
     ----------
     power_spectrum : 1d array
         Power values of the spectrum that is to be translated.
-    delta : float
+    delta_offset : float
         Amount to change the offset by.
         Positive is an upwards translation.
         Negative is a downwards translation.
@@ -53,12 +53,12 @@ def translate_spectrum(power_spectrum, delta):
         Translated power spectrum.
     """
 
-    translated_spectrum = np.power(10, delta, dtype='float') * power_spectrum
+    translated_spectrum = np.power(10, delta_offset, dtype='float') * power_spectrum
 
     return translated_spectrum
 
 
-def rotate_syn_spectrum(freqs, power_spectrum, delta, f_rotation, syn_params):
+def rotate_syn_spectrum(freqs, power_spectrum, delta_exponent, f_rotation, syn_params):
     """Rotate a power spectrum about a frequency point, changing the power law exponent.
 
     Parameters
@@ -67,7 +67,7 @@ def rotate_syn_spectrum(freqs, power_spectrum, delta, f_rotation, syn_params):
         Frequency axis of input power spectrum, in Hz.
     power_spectrum : 1d array
         Power values of the spectrum that is to be rotated.
-    delta : float
+    delta_exponent : float
         Change in aperiodic exponent to be applied.
         Positive is clockwise rotation (steepen).
         Negative is counterclockwise rotation (flatten).
@@ -84,22 +84,22 @@ def rotate_syn_spectrum(freqs, power_spectrum, delta, f_rotation, syn_params):
         Updated object storing the new parameter definitions.
     """
 
-    rotated_spectrum = rotate_spectrum(freqs, power_spectrum, delta, f_rotation)
-    delta_offset = calc_rot_offset(delta, f_rotation)
+    rotated_spectrum = rotate_spectrum(freqs, power_spectrum, delta_exponent, f_rotation)
+    delta_offset = calc_rot_offset(delta_exponent, f_rotation)
 
-    new_syn_params = update_syn_ap_params(syn_params, [delta_offset, delta])
+    new_syn_params = update_syn_ap_params(syn_params, [delta_offset, delta_exponent])
 
     return rotated_spectrum, new_syn_params
 
 
-def translate_syn_spectrum(power_spectrum, delta, syn_params):
+def translate_syn_spectrum(power_spectrum, delta_offset, syn_params):
     """Translate a spectrum, changing the offset value.
 
     Parameters
     ----------
     power_spectrum : 1d array
         Power values of the spectrum that is to be translated.
-    delta : float
+    delta_offset : float
         Amount to change the offset by.
         Positive is an upwards translation.
         Negative is a downwards translation.
@@ -114,18 +114,18 @@ def translate_syn_spectrum(power_spectrum, delta, syn_params):
         Updated object storing the new parameter definitions.
     """
 
-    translated_spectrum = translate_spectrum(power_spectrum, delta)
-    new_syn_params = update_syn_ap_params(syn_params, delta, 'offset')
+    translated_spectrum = translate_spectrum(power_spectrum, delta_offset)
+    new_syn_params = update_syn_ap_params(syn_params, delta_offset, 'offset')
 
     return translated_spectrum, new_syn_params
 
 
-def calc_rot_offset(delta, f_rotation):
+def calc_rot_offset(delta_exponent, f_rotation):
     """Calculate the change in offset from a given rotation.
 
     Parameters
     ----------
-    delta : float
+    delta_exponent : float
         Change in aperiodic exponent to be applied.
     f_rotation : float
         Frequency value, in Hz, about which rotation is applied, at which power is unchanged.
@@ -136,4 +136,4 @@ def calc_rot_offset(delta, f_rotation):
         The amount the offset will change for the specified exponent change.
     """
 
-    return -np.log10(f_rotation) * -delta
+    return -np.log10(f_rotation) * -delta_exponent
