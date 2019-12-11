65843270d0b1d5b8d893a2b0f359ca4f31d02e68
TomDonoghue <tdonoghue@ucsd.edu>
2019-12-11 01:23:59 -0800

    Add aperiodic param plots

---
+++ fooof/plts/aperiodic.py
@@ -0,0 +1,133 @@
+"""Plots for aperiodic fits and parameters."""
+
+import numpy as np
+
+from fooof.sim.gen import gen_freqs, gen_aperiodic
+from fooof.core.modutils import safe_import, check_dependency
+from fooof.plts.utils import check_ax, recursive_plot
+from fooof.plts.settings import FIGSIZE_PARAMS
+from fooof.plts.style import check_n_style, style_param_plot
+
+plt = safe_import('.pyplot', 'matplotlib')
+
+###################################################################################################
+###################################################################################################
+
+@check_dependency(plt, 'matplotlib')
+def plot_aperiodic_params(aps, colors=None, labels=None,
+                          ax=None, plot_style=style_param_plot, **plot_kwargs):
+    """Plot peaks as dots representing center frequency, power and bandwidth.
+
+    Parameters
+    ----------
+    aps : 2d array or list of 2d array
+        Aperiodic parameters. Each row is a parameter set, as [Off, Exp] or [Off, Knee, Exp].
+    colors : list of str, optional
+        Color(s) to plot data.
+    labels : list of str, optional
+        Label(s) for plotted data, to be added in a legend.
+    ax : matplotlib.Axes, optional
+        Figure axes upon which to plot.
+    plot_style : callable, optional, default: XX
+        A function to call to apply styling & aesthetics to the plot.
+    **plot_kwargs
+        Keyword arguments to pass into the plot call.
+    """
+
+    ax = check_ax(ax, FIGSIZE_PARAMS)
+
+    if isinstance(aps, list):
+        recursive_plot(aps, plot_aperiodic_params, ax, colors=colors, labels=labels,
+                       plot_style=plot_style, **plot_kwargs)
+
+    else:
+
+        xs, ys = aps[:, 0], aps[:, -1]
+        ax.scatter(xs, ys, 150, c=colors, label=labels,
+                   alpha=plot_kwargs.pop('alpha', 0.7), **plot_kwargs)
+
+    # Add axis labels
+    ax.set_xlabel('Offset')
+    ax.set_ylabel('Exponent')
+
+    check_n_style(plot_style, ax)
+
+
+@check_dependency(plt, 'matplotlib')
+def plot_aperiodic_fits(aps, freq_range, control_offset=False,
+                        log_freqs=False, colors=None, labels=None,
+                        ax=None, plot_style=style_param_plot, **plot_kwargs):
+    """Plot aperiodic components, comparing between groups.
+
+    Parameters
+    ----------
+    aps : 2d array
+        Aperiodic parameters. Each row is a parameter set, as [Off, Exp] or [Off, Knee, Exp].
+    freq_range : list of [float, float] , optional
+        The frequency range to plot the peak fits across, as [f_min, f_max].
+        If not provided, defaults to +/- 4 around given peak center frequencies.
+    control_offset : boolean, optonal, default: False
+        Whether to control for the offset, by setting it to zero.
+    log_freqs : boolean, optonal, default: False
+        Whether to plot the x-axis in log space.
+    colors : list of str, optional
+        Color(s) to plot data.
+    labels : list of str, optional
+        Label(s) for plotted data, to be added in a legend.
+    ax : matplotlib.Axes, optional
+        Figure axes upon which to plot.
+    plot_style : callable, optional, default: style_spectrum_plot
+        A function to call to apply styling & aesthetics to the plot.
+    **plot_kwargs
+        Keyword arguments to pass into the plot call.
+    """
+
+    ax = check_ax(ax, FIGSIZE_PARAMS)
+
+    if isinstance(aps, list):
+        recursive_plot(aps, plot_function=plot_aperiodic_fits, ax=ax,
+                       freq_range=tuple(freq_range),
+                       control_offset=control_offset,
+                       log_freqs=log_freqs, colors=colors, labels=labels,
+                       plot_style=plot_style, **plot_kwargs)
+    else:
+
+        freqs = gen_freqs(freq_range, 0.1)
+        plt_freqs = np.log10(freqs) if log_freqs else freqs
+
+        # Set default alpha & lw, tuned to whether it's one or two groups of data
+        alpha = plot_kwargs.get('alpha', 0.2) if colors is not None \
+            else plot_kwargs.get('alpha', 0.35)
+        lw = plot_kwargs.get('lw', 1.2) if colors is not None \
+            else plot_kwargs.get('lw', 1.5)
+
+        avg_vals = np.zeros(shape=[len(freqs)])
+
+        for ap_params in aps:
+
+            if control_offset:
+                ap_params = ap_params.copy()
+                ap_params[0] = 0
+
+            # Recreate & plot the aperiodic component from parameters
+            ap_vals = gen_aperiodic(freqs, ap_params)
+            ax.plot(plt_freqs, ap_vals, alpha=alpha, linewidth=lw, color=colors)
+
+            # Collect a running average across components
+            avg_vals = np.nansum(np.vstack([avg_vals, ap_vals]), axis=0)
+
+        # Plot the average component
+        avg = avg_vals / aps.shape[0]
+        avg_color = 'black' if not colors else colors
+        ax.plot(plt_freqs, avg, color=avg_color, linewidth=lw*3, label=labels)
+
+    # Add axis labels
+    ax.set_xlabel('Frequency (Hz)')
+    ax.set_ylabel('Power')
+
+    # Set plot limit
+    plt_range = np.log10(freq_range) if log_freqs else freq_range
+    ax.set_xlim(plt_range)
+
+    # Apply plot style
+    check_n_style(plot_style, ax)
