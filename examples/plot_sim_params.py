9e2d2c642abede57ea1c02ffb278c45832d63489
TomDonoghue <tdonoghue@ucsd.edu>
2020-01-29 09:48:47 -0800

    Add 'param_jitter to example

--- examples/plot_sim_params.py
+++ examples/plot_sim_params.py
@@ -8,7 +8,7 @@ Manage parameters for creating simulated power spectra.
 ###################################################################################################
 
 # Import fooof functions for creating spectra and managing parameters
-from fooof.sim.params import param_sampler, param_iter, Stepper
+from fooof.sim.params import param_sampler, param_iter, param_jitter, Stepper
 from fooof.sim.gen import gen_power_spectrum, gen_group_power_spectra
 
 # Import some fooof plotting functions
@@ -106,8 +106,13 @@ plot_spectra(fs, ps[0:4, :], log_powers=True)
 # Set the aperiodic parameters to be stable
 ap_params = [1, 1]
 
-# Use a stepper object and param_iter to step across CF values for an alpha oscillation
+# Use a stepper object to define the range of values to step across
+#   Stepper is defined with `start, stop, step`.
+#   Here we'll define a step across alpha center frequency values
 cf_steps = Stepper(8, 12, 1)
+
+# We can use use param_iter, with our Stepper object, to create the full peak params
+#   The other parameter values will be held constant as we step across CF values
 gauss_params = param_iter([cf_steps, 0.4, 1])
 
 ###################################################################################################
@@ -119,3 +124,37 @@ fs, ps, sim_params = gen_group_power_spectra(len(cf_steps), [3, 40], ap_params, 
 
 # Plot the generated spectra
 plot_spectra(fs, ps, log_freqs=True, log_powers=True)
+
+###################################################################################################
+# param_jitter
+# ~~~~~~~~~~~~
+#
+# The :func:`param_jitter` function can be used to create iterators that can sample from
+# sets of parameters, applying some jitter to them.
+#
+
+###################################################################################################
+
+# Define default aperiodic values, with some jitter
+#   The first input is the default values, the second the scale of the jitter
+#   You can set zero for any value that should not be jittered
+ap_params = param_jitter([1, 1], [0.0, 0.15])
+
+# Define the peak parameters, to be stable, with an alpha and a beta
+gauss_params = [10, 0.2, 1, 22, 0.1, 3]
+
+###################################################################################################
+
+# Generate some power spectra, using param jitter
+fs, ps, sim_params = gen_group_power_spectra(5, [3, 40], ap_params, gauss_params)
+
+###################################################################################################
+
+# Plot the generated spectra
+plot_spectra(fs, ps, log_freqs=True, log_powers=True)
+
+###################################################################################################
+#
+# We can see that in the generated spectra above, there is some jitter
+# to the simulated aperiodic exponent values.
+#
