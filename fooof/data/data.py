3b2bc7b6643388e516b41b5be7f8c12267b38847
TomDonoghue <tdonoghue@ucsd.edu>
2020-04-17 21:53:42 -0700

    rework data objects

--- fooof/data/data.py
+++ fooof/data/data.py
@@ -1,78 +1,84 @@
-"""Data objects for FOOOF."""
+"""Data objects for FOOOF.
+
+Notes on FOOOF data objects:
+- these data objects are NamedTuples, immutable data types with attribute labels
+- the namedtuples are wrapped as classes (they are still immutable when doing this)
+- wrapping in objects helps to be able to render well formed documentation for them.
+- setting `__slots__` as empty voids the dynamic dictionary that usually stores attributes
+    - this means no additional attributes can be defined (which is more memory efficient)
+"""
 
 from collections import namedtuple
 
 ###################################################################################################
 ###################################################################################################
 
-FOOOFSettings = namedtuple('FOOOFSettings', ['peak_width_limits', 'max_n_peaks',
-                                             'min_peak_height', 'peak_threshold',
-                                             'aperiodic_mode'])
-FOOOFSettings.__doc__ = """\
-The user defined settings for a FOOOF object.
+class FOOOFSettings(namedtuple('FOOOFSettings', ['peak_width_limits', 'max_n_peaks',
+                                                 'min_peak_height', 'peak_threshold',
+                                                 'aperiodic_mode'])):
+    """User defined settings for a FOOOF object.
 
-Attributes
-----------
-peak_width_limits : tuple of (float, float)
-    Limits on possible peak width, in Hz, as (lower_bound, upper_bound).
-max_n_peaks : int
-    Maximum number of peaks to fit.
-min_peak_height : float
-    Absolute threshold for detecting peaks, in units of the input data.
-peak_threshold : float
-    Relative threshold for detecting peaks, in units of standard deviation of the input data.
-aperiodic_mode : {'fixed', 'knee'}
-    Which approach to take for fitting the aperiodic component.
-"""
-
-
-FOOOFMetaData = namedtuple('FOOOFMetaData', ['freq_range', 'freq_res'])
-
-FOOOFMetaData.__doc__ = """\
-The meta-data for a FOOOF object.
-
-Attributes
-----------
-freq_range : list of [float, float]
-    Frequency range of the power spectrum, as [lowest_freq, highest_freq].
-freq_res : float
-    Frequency resolution of the power spectrum.
-"""
+    Parameters
+    ----------
+    peak_width_limits : tuple of (float, float)
+        Limits on possible peak width, in Hz, as (lower_bound, upper_bound).
+    max_n_peaks : int
+        Maximum number of peaks to fit.
+    min_peak_height : float
+        Absolute threshold for detecting peaks, in units of the input data.
+    peak_threshold : float
+        Relative threshold for detecting peaks, in units of standard deviation of the input data.
+    aperiodic_mode : {'fixed', 'knee'}
+        Which approach to take for fitting the aperiodic component.
+    """
+    __slots__ = ()
 
 
-FOOOFResults = namedtuple('FOOOFResults', ['aperiodic_params', 'peak_params',
-                                           'r_squared', 'error', 'gaussian_params'])
-FOOOFResults.__doc__ = """\
-The results from a FOOOF model fit.
+class FOOOFMetaData(namedtuple('FOOOFMetaData', ['freq_range', 'freq_res'])):
+    """Meta-data for a FOOOF object.
 
-Attributes
-----------
-aperiodic_params : 1d array
-    Parameters that define the aperiodic fit. As [Offset, (Knee), Exponent].
-    The knee parameter is only included if aperiodic is fit with knee.
-peak_params : 2d array
-    Fitted parameter values for the peaks. Each row is a peak, as [CF, PW, BW].
-r_squared : float
-    R-squared of the fit between the full model fit and the input data.
-error : float
-    Total error of the full model fit, compared to the input data.
-gaussian_params : 2d array
-    Parameters that define the gaussian fit(s).
-    Each row is a gaussian, as [mean, height, standard deviation].
-"""
+    Parameters
+    ----------
+    freq_range : list of [float, float]
+        Frequency range of the power spectrum, as [lowest_freq, highest_freq].
+    freq_res : float
+        Frequency resolution of the power spectrum.
+    """
+    __slots__ = ()
 
 
-SimParams = namedtuple('SimParams', ['aperiodic_params', 'periodic_params', 'nlv'])
+class FOOOFResults(namedtuple('FOOOFResults', ['aperiodic_params', 'peak_params',
+                                               'r_squared', 'error', 'gaussian_params'])):
+    """Model results from parameterizing a power spectrum.
 
-SimParams.__doc__ = """\
-Stores parameters used to simulate a single power spectra.
+    Attributes
+    ----------
+    aperiodic_params : 1d array
+        Parameters that define the aperiodic fit. As [Offset, (Knee), Exponent].
+        The knee parameter is only included if aperiodic is fit with knee.
+    peak_params : 2d array
+        Fitted parameter values for the peaks. Each row is a peak, as [CF, PW, BW].
+    r_squared : float
+        R-squared of the fit between the full model fit and the input data.
+    error : float
+        Error of the full model fit.
+    gaussian_params : 2d array
+        Parameters that define the gaussian fit(s).
+        Each row is a gaussian, as [mean, height, standard deviation].
+    """
+    __slots__ = ()
 
-Attributes
-----------
-aperiodic_params : list
-    Parameters that define the aperiodic component.
-periodic_params : list or list of lists
-    Parameters that define the periodic component.
-nlv : float
-    Noise level added to simulated spectrum.
-"""
+
+class SimParams(namedtuple('SimParams', ['aperiodic_params', 'periodic_params', 'nlv'])):
+    """Stores parameters that define a simulated power spectrum.
+
+    Attributes
+    ----------
+    aperiodic_params : list
+        Parameters that define the aperiodic component.
+    periodic_params : list or list of lists
+        Parameters that define the periodic component.
+    nlv : float
+        Noise level added to simulated spectrum.
+    """
+    __slots__ = ()
