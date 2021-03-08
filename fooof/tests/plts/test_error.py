201869e2e00681283ce9db983dd67cdec6e86e0a
ryanhammonds <ryan.hammonds@utexas.edu>
2021-03-08 11:25:41 -0800

    error shading tests

--- fooof/tests/plts/test_error.py
+++ fooof/tests/plts/test_error.py
@@ -1,5 +1,7 @@
 """Tests for fooof.plts.error."""
 
+from pytest import raises, mark, param
+
 import numpy as np
 
 from fooof.tests.tutils import plot_test
@@ -16,3 +18,20 @@ def test_plot_spectral_error(skip_if_no_mpl):
     errs = np.ones(len(fs))
 
     plot_spectral_error(fs, errs)
+
+
+@plot_test
+def test_plot_error_shade(skip_if_no_mpl, tfg):
+
+    freqs = tfg.freqs
+    powers = tfg.power_spectra
+
+    # Invalid 1d array, without shade
+    with raises(ValueError):
+        plot_error_shade(freqs, powers[0])
+
+    # Valid 1d array with shade
+    plot_error_shade(freqs, np.mean(powers, axis=0), shade=np.std(powers, axis=0))
+
+    # 2d array
+    plot_error_shade(freqs, powers)
