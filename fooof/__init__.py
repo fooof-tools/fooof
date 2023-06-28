65d54063250eea4618d68d724ea221eb2d0148dc
Bradley Voytek <bradley.voytek@gmail.com>
2023-06-28 15:55:46 -0700

    add deprecation warning

--- fooof/__init__.py
+++ fooof/__init__.py
@@ -2,6 +2,16 @@
 
 from .version import __version__
 
+# deprecation of fooof for specparam
+from warnings import warn, simplefilter
+simplefilter('always') # make sure user sees it once, on every import
+warn_text = 'Warning: The fooof package is being deprecated in favor of the\
+ spectral parameterization (specparam) package. This version of fooof is\
+ functional, however we recommend upgrading to specparam, because this 1.1\
+ version of fooof will not have continuing development. All new features and\
+ development will be done on the specparam package.'
+warn(warn_text, DeprecationWarning, stacklevel=2)
+
 from .bands import Bands
 from .objs import FOOOF, FOOOFGroup
 from .objs.utils import fit_fooof_3d
