a3e2953f8ec994aabb285d393cc9957a3f261426
Tom Donoghue <tdonoghue.research@gmail.com>
2023-07-07 16:09:09 -0400

    update test file dir management - drop pkg_resources & importlib

--- fooof/tests/settings.py
+++ fooof/tests/settings.py
@@ -1,20 +1,14 @@
 """Settings for testing fooof."""
 
 import os
-
-# Use importlib to get package related paths, importing depending on Python version
-#   Note: can drop if/else when support goes to >= 3.9 (see: https://stackoverflow.com/a/75503824)
-import sys
-if sys.version_info >= (3, 9):
-    import importlib.resources as importlib_resources
-else:
-    import importlib_resources
+from pathlib import Path
 
 ###################################################################################################
 ###################################################################################################
 
 # Path Settings
-BASE_TEST_FILE_PATH = importlib_resources.files(__name__.split('.')[0]) / 'tests/test_files'
-TEST_DATA_PATH = os.path.join(BASE_TEST_FILE_PATH, 'data')
-TEST_REPORTS_PATH = os.path.join(BASE_TEST_FILE_PATH, 'reports')
-TEST_PLOTS_PATH = os.path.join(BASE_TEST_FILE_PATH, 'plots')
+TESTS_PATH = Path(os.path.abspath(os.path.dirname(__file__)))
+BASE_TEST_FILE_PATH = TESTS_PATH / 'test_files'
+TEST_DATA_PATH = BASE_TEST_FILE_PATH / 'data'
+TEST_REPORTS_PATH = BASE_TEST_FILE_PATH / 'reports'
+TEST_PLOTS_PATH = BASE_TEST_FILE_PATH / 'plots'
