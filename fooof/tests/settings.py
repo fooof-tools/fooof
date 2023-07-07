a3f9d871122164f0bcd727c9e519ed1b5161c4a1
Tom Donoghue <tdonoghue.research@gmail.com>
2023-07-07 13:19:06 -0400

    replace package_resources with importlib

--- fooof/tests/settings.py
+++ fooof/tests/settings.py
@@ -1,13 +1,20 @@
 """Settings for testing fooof."""
 
 import os
-import pkg_resources as pkg
+
+# Use importlib to get package related paths, importing depending on Python version
+#   Note: can drop if/else when support goes to >= 3.9 (see: https://stackoverflow.com/a/75503824)
+import sys
+if sys.version_info >= (3, 9):
+    import importlib.resources as importlib_resources
+else:
+    import importlib_resources
 
 ###################################################################################################
 ###################################################################################################
 
 # Path Settings
-BASE_TEST_FILE_PATH = pkg.resource_filename(__name__, 'test_files')
+BASE_TEST_FILE_PATH = importlib_resources.files(__name__.split('.')[0]) / 'tests/test_files'
 TEST_DATA_PATH = os.path.join(BASE_TEST_FILE_PATH, 'data')
 TEST_REPORTS_PATH = os.path.join(BASE_TEST_FILE_PATH, 'reports')
 TEST_PLOTS_PATH = os.path.join(BASE_TEST_FILE_PATH, 'plots')
