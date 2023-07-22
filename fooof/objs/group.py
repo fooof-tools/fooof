825c4a48df34c2959a901c15946249c5ba84de51
Tom Donoghue <tdonoghue.research@gmail.com>
2023-07-22 12:47:38 -0400

    propagate modes when extracting sub-object from group

--- fooof/objs/group.py
+++ fooof/objs/group.py
@@ -458,7 +458,8 @@ class FOOOFGroup(FOOOF):
 
         # Initialize a FOOOF object, with same settings & check data mode as current FOOOFGroup
         fm = FOOOF(*self.get_settings(), verbose=self.verbose)
-        fm.set_check_data_mode(self._check_data)
+        fm.set_check_modes(self._check_freqs, self._check_data)
+        fm.set_debug_mode(self._debug)
 
         # Add data for specified single power spectrum, if available
         #   The power spectrum is inverted back to linear, as it is re-logged when added to FOOOF
@@ -496,6 +497,8 @@ class FOOOFGroup(FOOOF):
 
         # Initialize a new FOOOFGroup object, with same settings as current FOOOFGroup
         fg = FOOOFGroup(*self.get_settings(), verbose=self.verbose)
+        fg.set_check_modes(self._check_freqs, self._check_data)
+        fg.set_debug_mode(self._debug)
 
         # Add data for specified power spectra, if available
         #   The power spectra are inverted back to linear, as they are re-logged when added to FOOOF
