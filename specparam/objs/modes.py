5f0cca3c5995158fcafb653c7e58904036cd2f7f
Tom Donoghue <tdonoghue.research@gmail.com>
2023-08-13 22:53:56 -0400

    add new modes

--- specparam/objs/modes.py
+++ specparam/objs/modes.py
@@ -19,12 +19,15 @@ class Mode():
         self.freq_space = freq_space
         self.powers_space = powers_space
 
+
     def __repr__(self):
         return self.name
+
 
     @property
     def n_params(self):
         return len(self.params)
+
 
     @property
     def param_indices(self):
@@ -33,6 +36,7 @@ class Mode():
 ###################################################################################################
 ## APERIODIC MODES
 
+# Fixed
 param_desc_fixed = {
     'offset' : 'Offset of the aperiodic component.',
     'exponent' : 'Exponent of the aperiodic component.',
@@ -48,6 +52,7 @@ ap_fixed = Mode(
     powers_space='log10',
 )
 
+# Knee
 param_desc_knee = {
     'offset' : 'Offset of the aperiodic component.',
     'knee' : 'Knee of the aperiodic component.',
@@ -65,15 +70,37 @@ ap_knee = Mode(
     powers_space='log10',
 )
 
+
+# Double exponent
+param_desc = {
+    'offset' : 'Offset of the aperiodic component.',
+    'exponent0' : 'Exponent of the aperiodic component, before the knee.',
+    'knee' : 'Knee of the aperiodic component.',
+    'exponent1' : 'Exponent of the aperiodic component, after the knee.',
+    }
+
+ap_doublexp = Mode(
+    name='doublexp',
+    component='aperiodic',
+    description='Fit an function with 2 exponents and a knee.',
+    func=double_expo_function,
+    params=['offset', 'exponent0', 'knee', 'exponent1'],
+    param_description=param_desc,
+    freq_space='linear',
+    powers_space='log10',
+)
+
 # Collect available aperiodic modes
 AP_MODES = {
     'fixed' : ap_fixed,
     'knee' : ap_knee,
+    'doublexp' : ap_doublexp,
 }
 
 ###################################################################################################
 ## PERIODIC MODES
 
+# Gaussian
 param_desc_gaus = {
     'cf' : 'Center frequency of the peak.',
     'pw' : 'Power of the peak, over and above the aperiodic component.',
@@ -91,7 +118,27 @@ pe_gaussian = Mode(
     powers_space='log10',
 )
 
+# Skewed Gaussian
+param_desc_skew = {
+    'cf' : 'Center frequency of the peak.',
+    'pw' : 'Power of the peak, over and above the aperiodic component.',
+    'bw' : 'Bandwidth of the peak.',
+    'skew' : 'Skewness of the peak.',
+    }
+
+pe_skewnorm = Mode(
+    name='skewnorm',
+    component='periodic',
+    description='Skewed Gaussian peak fit function.',
+    func=skewnorm_function,
+    params=['cf', 'pw', 'bw', 'skew'],
+    param_description=param_desc_skew,
+    freq_space='linear',
+    powers_space='log10',
+)
+
 # Collect available periodic modes
 PE_MODES = {
     'gaussian' : pe_gaussian,
+    'skewed_gaussian' : pe_skewnorm,
 }
