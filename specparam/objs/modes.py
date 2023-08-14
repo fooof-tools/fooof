d046a3c4d3afe266afe0bb1655dddda7dd8f9bd2
Tom Donoghue <tdonoghue.research@gmail.com>
2023-08-13 22:59:00 -0400

    add docs to Mode object

--- specparam/objs/modes.py
+++ specparam/objs/modes.py
@@ -6,9 +6,32 @@ from specparam.core.funcs import *
 ## MODES OUTLINE
 
 class Mode():
+    """Defines a fit mode.
+
+    Parameters
+    ----------
+    name : str
+        Name of the mode.
+    component : {'periodic', 'aperiodic'},
+        Which component the mode relates to.
+    description : str
+        Description of the mode.
+    func : callable
+        Function that defines the fit function for the mode.
+    params : list of str
+        Name of output parameter(s).
+    param_description : dict
+        Descriptions of the parameters.
+        Should have same length and keys as `params`.
+    freq_space : {'linear', 'log10'}
+        Required spacing of the frequency values for this mode.
+    powers_space : {'linear', 'log10'}
+        Required spacing of the power values for this mode.
+    """
 
     def __init__(self, name, component, description, func, params, param_description,
                  freq_space, powers_space):
+        """Initialize a mode."""
 
         self.name = name
         self.component = component
@@ -21,16 +44,22 @@ class Mode():
 
 
     def __repr__(self):
+        """Return representation of this object as the name."""
+
         return self.name
 
 
     @property
     def n_params(self):
+        """Define property attribute for the number of parameters."""
+
         return len(self.params)
 
 
     @property
     def param_indices(self):
+        """Define property attribute for the indices of the parameters."""
+
         return {label : index for index, label in enumerate(self.params)}
 
 ###################################################################################################
