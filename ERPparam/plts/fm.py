2d32394f808fb4d3c95ec71aa324014e357f64b3
Michael <mpreston@ucsd.edu>
2023-08-29 15:23:17 -0700

    chore: rm plot_erp_param

--- ERPparam/plts/fm.py
+++ ERPparam/plts/fm.py
@@ -22,34 +22,6 @@ plt = safe_import('.pyplot', 'matplotlib')
 ###################################################################################################
 ###################################################################################################
 
-@check_dependency(plt, 'matplotlib')
-def plot_ERPparam(self, ax=None, y_label=None):
-    """Plot ERP and model fit results."""
-
-    # create figure
-    if ax is None:
-        fig, ax = plt.subplots(1,1, figsize=PLT_FIGSIZES['spectral'])
-
-    # plot signal
-    ax.plot(self.time, self.signal, alpha=0.5, label='ERP')
-
-    # plot fit
-    if self.peak_params_ is not None:
-        ax.plot(self.time, self._peak_fit, linestyle='--', color='k', label='Gaussian fit')
-        ax.scatter(self.peak_params_[:,0], self.peak_params_[:,1], color='r', label='Peak fit')
-    
-    # label
-    if y_label is not None:
-        ax.set(xlabel="time (s)", ylabel=y_label)
-    else:
-        ax.set(xlabel="time (s)", ylabel="amplitude")
-    ax.legend()
-
-    # style
-    style_erp_plot(ax)
-    plt.show()
-
-
 @savefig
 @style_plot
 @check_dependency(plt, 'matplotlib')
