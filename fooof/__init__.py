ad8a7bc35eccfb81a62e593476306fd3716b2308
Tom Donoghue <tdonoghue.research@gmail.com>
2023-09-13 08:32:58 -0400

    remove always warning filter from warning

--- fooof/__init__.py
+++ fooof/__init__.py
@@ -4,8 +4,7 @@ from .version import __version__
 
 # Deprecation of fooof / move to specparam message
 #   Note: this warning is for fooof v1.1 specifically, and should be removed in specparam 2.0
-from warnings import warn, simplefilter
-#simplefilter('always')  # make sure user sees it once, on every import
+from warnings import warn
 DEPRECATION_TEXT = ("\nThe `fooof` package is being deprecated and replaced by the "
     "`specparam` (spectral parameterization) package."
     "\nThis version of `fooof` (1.1) is fully functional, but will not be further updated."
