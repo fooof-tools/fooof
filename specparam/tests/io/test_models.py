a1772490434f618204c72f0648e56b072b0152f9
Tom Donoghue <tdonoghue.research@gmail.com>
2025-03-12 13:47:27 -0400

    consolidate IO code from duplication

--- specparam/tests/io/test_models.py
+++ specparam/tests/io/test_models.py
@@ -7,6 +7,8 @@ import os
 
 from specparam.core.items import OBJ_DESC
 from specparam.io.files import load_json
+from specparam.objs import (SpectralModel, SpectralGroupModel,
+                            SpectralTimeModel, SpectralTimeEventModel)
 
 from specparam.tests.tsettings import TEST_DATA_PATH
 
@@ -132,38 +134,6 @@ def test_save_event(tfe):
     for ind in range(len(tfe)):
         assert os.path.exists(TEST_DATA_PATH / (file_name_all + '_' + str(ind) + '.json'))
 
-def test_load_model():
-
-    # Loads file saved from `test_save_model_str`
-    file_name = 'test_model_all'
-
-    tmodel = load_model(file_name, TEST_DATA_PATH)
-    assert tmodel
-
-def test_load_group():
-
-    # Loads file saved from `test_save_group`
-    file_name = 'test_group_all'
-
-    tgroup = load_group(file_name, TEST_DATA_PATH)
-    assert tgroup
-
-def test_load_time():
-
-    # Loads file saved from `test_save_time`
-    file_name = 'test_time_all'
-
-    ttime = load_time(file_name, TEST_DATA_PATH)
-    assert ttime
-
-def test_load_event():
-
-    # Loads file saved from `test_save_event`
-    file_name = 'test_event_all'
-
-    tevent = load_event(file_name, TEST_DATA_PATH)
-    assert tevent
-
 def test_load_file_contents():
     """Check that loaded model files contain the contents they should."""
 
@@ -178,3 +148,69 @@ def test_load_file_contents():
         assert result in loaded_data.keys()
     for datum in OBJ_DESC['data']:
         assert datum in loaded_data.keys()
+
+def test_load_model():
+
+    # Loads file saved from `test_save_model_str`
+    file_name = 'test_model_all'
+
+    tfm = load_model(file_name, TEST_DATA_PATH)
+
+    assert isinstance(tfm, SpectralModel)
+
+    # Check that all elements get loaded
+    for result in OBJ_DESC['results']:
+        assert not np.all(np.isnan(getattr(tfm, result)))
+    for setting in OBJ_DESC['settings']:
+        assert getattr(tfm, setting) is not None
+    for data in OBJ_DESC['data']:
+        assert getattr(tfm, data) is not None
+    for meta_dat in OBJ_DESC['meta_data']:
+        assert getattr(tfm, meta_dat) is not None
+
+def test_load_group():
+
+    # Loads file saved from `test_save_group`
+    file_name = 'test_group_all'
+
+    tfg = load_group(file_name, TEST_DATA_PATH)
+
+    assert isinstance(tfg, SpectralGroupModel)
+
+    # Check that all elements get loaded
+    assert len(tfg.group_results) > 0
+    for setting in OBJ_DESC['settings']:
+        assert getattr(tfg, setting) is not None
+    assert tfg.power_spectra is not None
+    for meta_dat in OBJ_DESC['meta_data']:
+        assert getattr(tfg, meta_dat) is not None
+
+def test_load_time(tbands):
+
+    # Loads file saved from `test_save_time`
+    file_name = 'test_time_all'
+
+    # Load without bands definition
+    tft = load_time(file_name, TEST_DATA_PATH)
+    assert isinstance(tft, SpectralTimeModel)
+
+    # Load with bands definition
+    tft2 = load_time_model(file_name, TEST_DATA_PATH, tbands)
+    assert isinstance(tft2, SpectralTimeModel)
+    assert tft2.time_results
+
+def test_load_event(tbands):
+
+    # Loads file saved from `test_save_event`
+    file_name = 'test_event_all'
+
+    # Load without bands definition
+    tfe = load_event(file_name, TEST_DATA_PATH)
+    assert isinstance(tfe, SpectralTimeEventModel)
+    assert len(tfe) > 1
+
+    # Load with bands definition
+    tfe2 = load_event(file_name, TEST_DATA_PATH, tbands)
+    assert isinstance(tfe2, SpectralTimeEventModel)
+    assert tfe2.event_time_results
+    assert len(tfe2) > 1
