9d39303cce7989e9f5a44235d0553093489c47f7
Tom Donoghue <tdonoghue.research@gmail.com>
2025-03-12 10:25:50 -0400

    fix import

--- specparam/tests/tutils.py
+++ specparam/tests/tutils.py
@@ -2,7 +2,7 @@
 
 from functools import wraps
 
-from specparam.core.modutils import safe_import
+from specparam.modutils.dependencies import safe_import
 
 plt = safe_import('.pyplot', 'matplotlib')
 
