26f22da8f4c2a4342f997bd005593ba64f5e133c
Tom Donoghue <tdonoghue.research@gmail.com>
2025-03-21 23:18:24 -0400

    move algo settings to own file

--- specparam/tests/algorithms/test_algorithm.py
+++ specparam/tests/algorithms/test_algorithm.py
@@ -8,22 +8,6 @@ from specparam.algorithms.algorithm import *
 ###################################################################################################
 ###################################################################################################
 
-def test_settings_definition():
-
-    tsettings = {
-        'a' : {'type' : 'a type desc', 'description' : 'a desc'},
-        'b' : {'type' : 'b type desc', 'description' : 'b desc'},
-    }
-
-    settings = SettingsDefinition(tsettings)
-    assert settings._settings == tsettings
-    assert settings.names == list(tsettings.keys())
-    assert settings.types
-    assert settings.descriptions
-    for label in tsettings.keys():
-        assert settings.make_setting_str(label)
-    assert settings.make_docstring()
-
 def test_algorithm_definition():
 
     tname = 'test_algo'
