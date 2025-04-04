b8d5f213c03d3a460cbaab9193f14859a032ed47
Tom Donoghue <tdonoghue.research@gmail.com>
2025-04-04 13:20:02 -0400

    drop get_periodic_labels

--- specparam/data/utils.py
+++ specparam/data/utils.py
@@ -105,30 +105,6 @@ def get_group_params(group_results, name, col=None):
     return out
 
 
-def get_periodic_labels(results):
-    """Get labels of periodic fields from a dictionary representation of parameter results.
-
-    Parameters
-    ----------
-    results : dict
-        A results dictionary with parameter label keys and corresponding parameter values.
-
-    Returns
-    -------
-    dict
-        Dictionary indicating the periodic related labels from the input results.
-        Has keys ['cf', 'pw', 'bw'] with corresponding values of related labels in the input.
-    """
-
-    keys = list(results.keys())
-
-    outs = {}
-    for label in ['cf', 'pw', 'bw']:
-        outs[label] = [key for key in keys if label in key]
-
-    return outs
-
-
 def get_results_by_ind(results, ind):
     """Get a specified index from a dictionary of results.
 
