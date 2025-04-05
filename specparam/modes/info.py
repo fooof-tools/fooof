20151ecada7dc8d078155be147d19c8dbf22e53b
Tom Donoghue <tdonoghue.research@gmail.com>
2025-04-05 01:23:46 -0400

    remove get_indices

--- specparam/modes/info.py
+++ specparam/modes/info.py
@@ -2,26 +2,3 @@
 
 ###################################################################################################
 ###################################################################################################
-
-def get_indices(aperiodic_mode):
-    """Get a mapping from column labels to indices for all parameters.
-
-    Parameters
-    ----------
-    aperiodic_mode : {'fixed', 'knee'}
-        Which mode was used for the aperiodic component.
-
-    Returns
-    -------
-    indices : dict
-        Mapping of the column labels and indices for all parameters.
-    """
-
-    # TEMP / TEST:
-    aperiodic_mode = str(aperiodic_mode)
-
-    # Get the periodic indices, and then update dictionary with aperiodic ones
-    indices = get_peak_indices()
-    indices.update(get_ap_indices(aperiodic_mode))
-
-    return indices
--- specparam/tests/modes/test_info.py
+++ specparam/tests/modes/test_info.py
@@ -4,11 +4,3 @@ from specparam.modes.info import *
 
 ###################################################################################################
 ###################################################################################################
-
-def test_get_indices():
-
-    all_indices_fixed = get_indices('fixed')
-    assert len(all_indices_fixed) == 5
-
-    all_indices_knee = get_indices('knee')
-    assert len(all_indices_knee) == 6
