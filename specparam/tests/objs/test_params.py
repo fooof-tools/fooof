9e194c2b9bb9c370a564f959c3176b10f06e4e71
Tom Donoghue <tdonoghue.research@gmail.com>
2025-10-25 13:21:12 +0100

    add tests for CParams

--- specparam/tests/objs/test_params.py
+++ specparam/tests/objs/test_params.py
@@ -1,5 +1,7 @@
 """Tests for specparam.objs.params."""
 
+import numpy as np
+
 from specparam.objs.params import *
 
 ###################################################################################################
@@ -11,3 +13,32 @@ def test_model_parameters():
 
     mp = ModelParameters()
     assert mp
+
+## ComponentParameters object
+
+def test_component_parameters():
+
+    acp = ComponentParameters('aperiodic')
+    assert acp
+
+    # Check initializing
+    acp.initialize(2)
+    assert len(acp._fit) == 2
+    assert len(acp._converted) == 2
+
+    # Check adding values
+    fparams = np.array([1, 2])
+    acp.add_params('fit', fparams)
+    assert acp.has_fit
+    assert np.array_equal(acp.params, fparams)
+
+    cparams = np.array([3, 4])
+    acp.add_params('converted', cparams)
+    assert acp.has_converted
+    assert np.array_equal(acp.params, cparams)
+
+    # Check dictionary export
+    pdict = acp.asdict()
+    assert isinstance(pdict, dict)
+    assert np.array_equal(pdict['aperiodic_fit'], fparams)
+    assert np.array_equal(pdict['aperiodic_converted'], cparams)
