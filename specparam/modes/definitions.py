7479d7e8a164b3d017342d4e31109a245f6be64f
Tom Donoghue <tdonoghue.research@gmail.com>
2025-10-29 18:24:16 +0000

    add check_modes func

--- specparam/modes/definitions.py
+++ specparam/modes/definitions.py
@@ -158,3 +158,24 @@ MODES = {
     'aperiodic' : AP_MODES,
     'periodic' : PE_MODES,
 }
+
+
+def check_modes(component, check_params=False):
+    """Check the set of modes that are available.
+
+    Parameters
+    ----------
+    component : {'aperiodic', 'periodic'}
+        Which component to check available modes for.
+    check_params : bool, optional, default: False
+        Whether to print out information on the parameters of each mode.
+    """
+
+    print('Available {:s} modes:'.format(component))
+    for mode in ALL_MODES[component].values():
+        if not check_params:
+            print('    {:10s}    {:s}'.format(mode.name, mode.description))
+        else:
+            print('\n{:s}'.format(mode.name))
+            print('    {:s}'.format(mode.description))
+            mode.check_params()
