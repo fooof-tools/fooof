018983c410bcfb805aa6a5c07bb05ab31cf98be4
Tom Donoghue <tdonoghue.research@gmail.com>
2025-11-29 17:14:40 +0000

    add tests for convert sub-module

---
+++ specparam/tests/convert/test_definitions.py
@@ -0,0 +1,19 @@
+"""Test functions for specparam.convert.definitions."""
+
+from specparam.modes.mode import VALID_COMPONENTS
+from specparam.convert.converter import BaseParamConverter
+
+from specparam.convert.definitions import *
+
+###################################################################################################
+###################################################################################################
+
+def test_converters_library():
+
+    for component in VALID_COMPONENTS:
+        for parameter, converters in CONVERTERS[component].items():
+            for label, converter in converters.items():
+                assert isinstance(converter, BaseParamConverter)
+                assert converter.component == component
+                assert converter.name == label
+                assert callable(converter.function)
