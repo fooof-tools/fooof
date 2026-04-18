4b587f0ce73bd51c7b399cf51336bb02ccf58e8b
Tom Donoghue <tdonoghue.research@gmail.com>
2026-04-19 00:29:27 +0100

    update check_metrics implementation

--- specparam/metrics/check.py
+++ specparam/metrics/check.py
@@ -1,5 +1,8 @@
 """Functionality to check available metrics."""
 
+from specparam.reports.settings import LCV
+from specparam.reports.strings import gen_metrics_str
+
 ###################################################################################################
 ###################################################################################################
 
@@ -12,11 +15,18 @@ def check_metrics(category='all'):
         Which category of metrics to check.
     """
 
+    from specparam.metrics.metrics import Metrics
     from specparam.metrics.definitions import METRICS
 
     category = ['error', 'gof'] if category == 'all' else [category]
 
+    out = ''
     for cat in category:
-        print('Available {} metrics:'.format(cat))
-        for metric in METRICS[cat].values():
-            print('    {:15s}    {:s}'.format(metric.measure, metric.description))
+
+        met_str = gen_metrics_str(Metrics(METRICS[cat].values()), True)
+        met_str = met_str.replace('CURRENT METRICS', 'AVAILABLE {} METRICS'.format(cat.upper()))
+        out += met_str
+
+    out = out.replace('\n' + '=' * LCV * 2 + '\n', '')
+
+    print(out)
