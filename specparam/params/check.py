34ff62a8ff1384f6e974da93e051572ab1a7b618
Tom Donoghue <tdonoghue.research@gmail.com>
2026-06-10 18:36:50 +0100

    refactor params check

--- specparam/params/check.py
+++ specparam/params/check.py
@@ -1,21 +1,32 @@
 """Functionality to check available parameter conversions."""
 
+from specparam.reports.strings import _format
+from specparam.modes.mode import VALID_COMPONENTS
+
 ###################################################################################################
 ###################################################################################################
 
-def check_converters(component):
+def check_converters(component='all', concise=False):
     """Check the set of parameter converters that are available.
 
     Parameters
     ----------
-    component : {'aperiodic', 'periodic'}
+    component : {'all', 'aperiodic', 'periodic'}
         Which component to check available parameter converters for.
     """
 
     from specparam.params.definitions import CONVERTERS
 
-    print('Available {:s} converters:'.format(component))
-    for param, convs in CONVERTERS[component].items():
-        print(param)
-        for label, converter in convs.items():
-            print('    {:10s}    {:s}'.format(converter.name, converter.description))
+    components = VALID_COMPONENTS if component == 'all' else [component]
+
+    str_lst = []
+    for component in components:
+
+        str_lst.extend(['', 'AVAILABLE {} PARAMETER CONVERTERS'.format(component.upper()), ''])
+
+        for param, convs in CONVERTERS[component].items():
+            str_lst.append("'" + param + "'")
+            for label, converter in convs.items():
+                str_lst.append('{:s}: {:s}'.format(converter.name, converter.description))
+
+    print(_format(str_lst[1:], concise))
