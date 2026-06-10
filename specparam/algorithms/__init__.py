d03a9d9ea21160738f670e09b37866c312754495
Tom Donoghue <tdonoghue.research@gmail.com>
2026-06-10 18:52:41 +0100

    move & refactor algo check func

--- specparam/algorithms/__init__.py
+++ specparam/algorithms/__init__.py
@@ -1,1 +1,4 @@
 """Algorithms sub-module."""
+
+from .algorithm import Algorithm
+from .check import check_algorithms
---
+++ specparam/algorithms/check.py
@@ -0,0 +1,20 @@
+"""Functionality to check available algorithm definitions."""
+
+from specparam.reports.strings import _format
+
+###################################################################################################
+###################################################################################################
+
+def check_algorithms(concise=False):
+    """Check the set of available fit algorithms."""
+
+    from specparam.algorithms.definitions import ALGORITHMS
+
+    str_lst = []
+    str_lst.extend(['', 'AVAILABLE ALGORITHMS', ''])
+
+    for algorithm in ALGORITHMS.values():
+        algorithm = algorithm()
+        str_lst.append('{:s} : {:s}'.format(algorithm.name, algorithm.description))
+
+    print(_format(str_lst[1:], concise))
